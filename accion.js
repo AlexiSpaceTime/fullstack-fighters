a40a4f43e845ec4604733d48b0277b6d90af9416
AlexiSpaceTime <alexis.fernandez@genstudents.org>
2024-09-06 16:11:29 -0400

    Comentarios en HTML, CSS, JS

--- accion.js
+++ accion.js
@@ -1,13 +1,14 @@
 
 
-let form = document.getElementById('cta-form');
+let form = document.getElementById('cta-form'); //Obtiene el elemento HTML con su identificación
 
-form.addEventListener('click', function() {
-  let formElement = document.getElementById('form');
-formElement.scrollIntoView({ behavior: 'smooth' });
+form.addEventListener('click', function() { // Añade un "escuchador de eventos" al botón form, que ejecuta una función cada vez que el botón es clicado
+  let formElement = document.getElementById('form'); //Obtiene el elemento HTML con el identificador form
+formElement.scrollIntoView({ behavior: 'smooth' }); // Desplaza suavemente hasta el elemento form
 });
+;
 
-  function presionarBoton() {
+  function presionarBoton() { //Define una función llamada "presionarBoton" que se ejecutará cuando se haga clic en el botón de enviar del formulario
     // capturar los valores de los inputs
     let nombre = document.getElementById('name').value;
     let email = document.getElementById('email').value;
